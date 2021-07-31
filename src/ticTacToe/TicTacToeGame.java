8a70286b1191dd829b97ab6485c9766a56992837
vismayatk002 <vismayatk002@gmail.com>
2021-08-01 01:03:46 +0530

    Add : Option to choose letter

--- src/ticTacToe/TicTacToeGame.java
+++ src/ticTacToe/TicTacToeGame.java
@@ -1,8 +1,11 @@
 package ticTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	
 	char[] gamearr = new char[10];
+	Scanner sc = new Scanner(System.in);
 	//method for clearing TicTacToe game board
 	public void clearGameBoard(){
 		
@@ -12,9 +15,24 @@ public class TicTacToeGame {
 			i++;
 		}
 	}
+	//return type character
+	public char chooseLetter(){
+		
+		System.out.print("Choose your letter X or O : ");
+		char letter = sc.next().charAt(0);
+		//if input not equal to X or O then returns '' 
+		if(letter != 'X' && letter != 'O'){
+			System.out.println("Invalid Input");
+			letter = ' ';
+		}
+		return letter;
+	}
 	public static void main(String[] args) {
 		
-		TicTacToeGame t = new TicTacToeGame();
-		t.clearGameBoard();
+		TicTacToeGame tg = new TicTacToeGame();
+		tg.clearGameBoard();
+		char letter = tg.chooseLetter();
+		System.out.println("Player Choose the letter : " + letter);
+		
 	}
 }

f8ec5aa77a8898a9a2b079394d2c2d1f5d197274
vismayatk002 <vismayatk002@gmail.com>
2021-07-31 22:43:39 +0530

    Add : created clear game board method

--- src/ticTacToe/TicTacToeGame.java
+++ src/ticTacToe/TicTacToeGame.java
@@ -2,8 +2,19 @@ package ticTacToe;
 
 public class TicTacToeGame {
 	
+	char[] gamearr = new char[10];
+	
+	public void emptyArr(){
+		
+		int i = 1; 
+		while(i < 10) {
+			gamearr[i] = ' ';
+			i++;
+		}
+	}
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to TicTacToe Game");
+		TicTacToeGame t = new TicTacToeGame();
+		t.emptyArr();
 	}
 }

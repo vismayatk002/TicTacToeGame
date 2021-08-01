99af9ca66e6f7ee2cfb14b928f170090c9bc28c5
vismayatk002 <vismayatk002@gmail.com>
2021-08-01 16:12:37 +0530

    Add : method to display TicTacToeGame board

--- src/ticTacToe/TicTacToeGame.java
+++ src/ticTacToe/TicTacToeGame.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	
-	char[] gamearr = new char[10];
 	Scanner sc = new Scanner(System.in);
+	char[] gamearr = new char[10];
+	
 	//method for clearing TicTacToe game board
 	public void clearGameBoard(){
 		
@@ -15,7 +16,7 @@ public class TicTacToeGame {
 			i++;
 		}
 	}
-	//return type character
+	//method for reading a letter 
 	public char chooseLetter(){
 		
 		System.out.print("Choose your letter X or O : ");
@@ -27,12 +28,25 @@ public class TicTacToeGame {
 		}
 		return letter;
 	}
+	//method for print TicTocToeBoard
+	public void showBoard(){
+		
+		System.out.println("Your TicTacToe board : ");
+		for(int i=1; i<10; i++) {
+			System.out.print(gamearr[i] + " ");
+			//print value in new line when position reaches 3 & 6
+			if(i == 3 || i == 6){
+				System.out.println("\n");
+			}
+		}
+	}
 	public static void main(String[] args) {
 		
-		TicTacToeGame tg = new TicTacToeGame();
-		tg.clearGameBoard();
-		char letter = tg.chooseLetter();
+		TicTacToeGame game = new TicTacToeGame();
+		game.clearGameBoard();
+		char letter = game.chooseLetter();
 		System.out.println("Player Choose the letter : " + letter);
+		game.showBoard();
 		
 	}
 }
